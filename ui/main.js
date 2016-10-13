db02411b63e0f4a200a8d4f78546b0cbf4f8f66c
gnanavalli <gvsnthl@gmail.com>
2016-10-13 16:52:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,10 +28,10 @@ button.onclick = function()
 }
 
 // submit name
-var nameInput = document.getElementById("name");
+var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
-submit.onclcik = function()
+submit.onclick = function()
 {
     var names= ["name1","name2","name3","name4" ];
     var list = '';
