c3a3b4978f7462d522047553161f11e724f84855
gnanavalli <gvsnthl@gmail.com>
2016-10-13 14:01:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,8 +22,9 @@ app.get('/ui/main.js', function (req, res) {
 });
 var counter=0;
 app.get('/counter', function (req, res) {
-    counter counter + 1;
-  res.send(counter.tostring());
+    
+    counter = counter+ 1 ;
+    res.send(counter.tostring());
 });
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
