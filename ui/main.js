c42e992cc59121e644598d68f4198d43e911d706
gnanavalli <gvsnthl@gmail.com>
2016-10-13 15:13:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,28 @@
-var counter=0;
+
 var button= document.getElementById("counter");
 button.onclick = function()
 {
     
-    //render the variable in the counter span
-    counter = counter + 1;
-    var span = document.getElementById("count");
-    span.innerHTML = counter.toString();
+    //Create request to the counter endpoing
+    request = new XMLHttpRequest();
+    
+    //capture the response and store it in the variable
+    request.onreadystatechanged = function() {
+        
+            if (request.readyState === XMLHttpRequest.DONE)
+            {
+                //take some action
+                if (request.status === 200)
+                {
+                    var counter = request.responseText;
+                    var span = document.getElementById("count");
+                    span.innerHTML = counter.toString();
+                }
+            }
+    };
+    //Make the request
+    rquest.open('GET','http://gnanavalli.imad.hasura-app.io/counter',true);
+    request.send(null);
+   
 }
 
