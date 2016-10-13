062e5ccce5cfa515cb6662632ff65754ca3d4e5e
gnanavalli <gvsnthl@gmail.com>
2016-10-13 22:21:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 var names=[];
-app.get('/submit-name/:name', function (req, res) {
+app.get('/submit-name', function (req, res) {
     //var name = req.params.name;
     var name = req.query.name;
     names.push(name);
