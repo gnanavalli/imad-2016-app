f2b5f0c87e6d0575b02504eaadd937416cb444e5
gnanavalli <gvsnthl@gmail.com>
2016-10-13 13:11:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,15 @@ element.innerHTML = 'New Value Test';
 
 // move the image
 var img= document.getElementById('madi');
-img.onclick = function() {
- img.style.marginLeft = '100px'   ;
+
+var marginLeft=0;
+function moveRight()
+{
+    marginLeft= marginLeft + 1;
+    img.style.marginLeft = marginLeft + 'px';
 }
+
+img.onclick = function() {
+ var interval = setInterval(moveRight,50);
+}
+
