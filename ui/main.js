09389792a858c84d822f4fdbec6ac55ccc7682bc
gnanavalli <gvsnthl@gmail.com>
2016-10-13 22:39:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,9 +15,18 @@ button.onclick = function()
                 //take some action
                 if (request.status === 200)
                 {
-                    var counter = request.responseText;
-                    var span = document.getElementById("count");
-                    span.innerHTML = counter.toString();
+                   // var counter = request.responseText;
+                   // var span = document.getElementById("count");
+                   // span.innerHTML = counter.toString();
+                   
+                   var names= ["name1","name2","name3","name4" ];
+                    var list = ' ';
+                     for(var i=0;i<names.length;i++)
+                     {
+                       list += '<li>' + names[i] + '</li>';
+                     }
+                    var ul = document.getElementById('namelist');
+                    ul.innerHTML = list;
                 }
             }
     };
@@ -27,18 +36,46 @@ button.onclick = function()
    
 }
 
+
 // submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function()
 {
-    var names= ["name1","name2","name3","name4" ];
-    var list = ' ';
-    for(var i=0;i<names.length;i++)
-    {
-        list += '<li>' + names[i] + '</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
-};
+    //Create request to the counter endpoing
+    var request = new XMLHttpRequest();
+    
+    //capture the response and store it in the variable
+    request.onreadystatechange = function() {
+        
+            if (request.readyState === XMLHttpRequest.DONE)
+            {
+                //take some action
+                if (request.status === 200)
+                {
+                   // var counter = request.responseText;
+                   // var span = document.getElementById("count");
+                   // span.innerHTML = counter.toString();
+                   
+                   var names= ["name1","name2","name3","name4" ];
+                    var list = ' ';
+                     for(var i=0;i<names.length;i++)
+                     {
+                       list += '<li>' + names[i] + '</li>';
+                     }
+                    var ul = document.getElementById('namelist');
+                    ul.innerHTML = list;
+                }
+            }
+    };
+    
+    //Make the request
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
+    request.open('GET','http://gnanavalli.imad.hasura-app.io/submit-name?name=' + name ,true);
+    request.send(null);
+   
+}
+
+
+
