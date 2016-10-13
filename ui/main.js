5df80bff05e4e9e58dc01ffc621b9db9df6a6422
gnanavalli <gvsnthl@gmail.com>
2016-10-13 16:24:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -33,7 +33,7 @@ var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclcik = function()
 {
-    var names[] = ["name1","name2","name3"];
+    var names= ["name1","name2","name3","name4" ];
     var list = '';
     for(vari=0;i<names.length;i++)
     {
