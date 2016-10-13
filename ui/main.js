6a989ac30f0f74e01fdb3c0651bd0d8c9f86675b
gnanavalli <gvsnthl@gmail.com>
2016-10-13 15:14:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,7 +21,7 @@ button.onclick = function()
             }
     };
     //Make the request
-    rquest.open('GET','http://gnanavalli.imad.hasura-app.io/counter',true);
+    request.open('GET','http://gnanavalli.imad.hasura-app.io/counter',true);
     request.send(null);
    
 }
